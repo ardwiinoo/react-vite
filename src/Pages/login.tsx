185e19e2c5ff4ff1595b14445377436e97fe6962
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-08-13 08:55:49 +0700

    add link to register

--- src/Pages/login.tsx
+++ src/Pages/login.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import FormLogin from "../components/Fragments/FormLogin"
 import AuthLayout from "../components/Layouts/AuthLayout"
 
@@ -5,6 +6,10 @@ const LoginPage = () => {
     return (
         <AuthLayout title="Login">
             <FormLogin />
+            <p className="text-sm mt-5 text-center">
+                Don't have account? 
+                <Link to="/register" className="px-2 font-bold text-blue-600">Register Here</Link>
+            </p>
         </AuthLayout>
     )
 }
