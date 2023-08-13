85fe86b2bceb0ceb7219f9058f6b1251153b0a6b
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-08-13 08:56:57 +0700

    adjust div to flex

--- src/components/Layouts/AuthLayout.tsx
+++ src/components/Layouts/AuthLayout.tsx
@@ -5,12 +5,14 @@ interface Props {
 
 const AuthLayout = (props: Props) => {
     return (
-        <div className="w-full max-w-xs">
-            <h1 className="text-blue-600 text-3xl font-bold mb-2">{props.title}</h1>
-            <p className="font-medium text-slate-500 mb-8">
-                Welcome, please enter your details...
-            </p>
-            {props.children}
+        <div className="flex justify-center min-h-screen items-center">
+            <div className="w-full max-w-xs">
+                <h1 className="text-blue-600 text-3xl font-bold mb-2">{props.title}</h1>
+                <p className="font-medium text-slate-500 mb-8">
+                    Welcome, please enter your details...
+                </p>
+                {props.children}
+            </div>
         </div>
     )
 }
