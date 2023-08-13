dfebbd03520722b37dec5c8963a76e586ce9667d
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-08-13 13:10:37 +0700

    use conditional rendering

--- src/components/Layouts/AuthLayout.tsx
+++ src/components/Layouts/AuthLayout.tsx
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom"
+
 interface Props {
     children: React.ReactNode,
-    title: string
+    title: string,
+    type: string
 }
 
 const AuthLayout = (props: Props) => {
@@ -12,6 +15,22 @@ const AuthLayout = (props: Props) => {
                     Welcome, please enter your details...
                 </p>
                 {props.children}
+                <p className="text-sm mt-5 text-center">
+                
+                {/* using Ternary */}
+                {props.type === 'login' ? 'Dont have an account?' : 'Already have an account?'}
+                
+                {/* using single condition */}
+                {props.type === 'login' && (
+                    <Link to="/register" className="px-2 font-bold text-blue-600">Register Here</Link>
+                )}
+
+                {props.type === 'register' && (
+                    <Link to="/login" className="px-2 font-bold text-blue-600">Login Here</Link>
+                )}
+
+                {/* Bisa juga pakai if else */}
+            </p>
             </div>
         </div>
     )
