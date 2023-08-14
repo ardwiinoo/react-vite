7dae6116a434180b7fbbd7a33cb26770b9fa6ca6
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-08-14 10:00:21 +0700

    list rendering

--- src/components/Fragments/CardProduct.tsx
+++ src/components/Fragments/CardProduct.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const CardProduct = (props: Props) => {
     return (
-        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2">
+        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between">
             {props.children}
         </div>
     )
@@ -34,7 +34,7 @@ interface BodyProps {
 
 const Body = (props: BodyProps) => {
     return (
-        <div className="px-5 pb-5">
+        <div className="px-5 pb-5 h-full">
             <a href="">
                 <h5 className="text-xl font-semibold tracking-tight text-white">
                     {props.title}
