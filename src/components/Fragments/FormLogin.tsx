29b7036d670aeea27e08e4e5b35f51e1aef1e1f5
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-08-14 10:00:40 +0700

    try handle login using FormEvent

--- src/components/Fragments/FormLogin.tsx
+++ src/components/Fragments/FormLogin.tsx
@@ -2,17 +2,33 @@ import InputForm from '../Elements/Input/index';
 import Button from '../Elements/Button/index';
 
 const FormLogin = () => {
+
+    // event handler
+    const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        const email = (event.currentTarget.elements.namedItem('email') as HTMLInputElement).value
+        const password = (event.currentTarget.elements.namedItem('password') as HTMLInputElement).value
+
+        // local storage
+        localStorage.setItem('email', email)
+        localStorage.setItem('password', password)
+        
+        window.location.href = '/products'
+    }
+
     return (
-        <form action="">
+        <form onSubmit={handleLogin}>
             <InputForm 
                 inputProps={{ name: 'email', type: 'text', placeholder: 'Email' }}
                 labelProps={{ text: 'Email', htmlFor: 'email' }}  />
             <InputForm 
-                inputProps={{ name: 'password', type: 'password', placeholder: 'password' }}
+                inputProps={{ name: 'password', type: 'password', placeholder: '******' }}
                 labelProps={{ text: 'password', htmlFor: 'password' }}  />
             <Button
                 title="Login"
                 variant="bg-blue-500"
+                type="submit"
             ></Button>
         </form>
     )
