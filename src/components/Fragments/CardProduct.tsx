5dfd6f0c7be4f40ea0bea67b2d7a7ae238d51fc2
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-08-15 08:40:54 +0700

    change currency IDR to USD

--- src/components/Fragments/CardProduct.tsx
+++ src/components/Fragments/CardProduct.tsx
@@ -22,7 +22,7 @@ const Header = (props: HeaderProps) => {
             <img 
                 src={props.image} 
                 alt="products" 
-                className="p-8 rounded-t-lg" />
+                className="p-8 rounded-t-lg w-full h-64 object-cover" />
         </a>
     )
 }
@@ -55,7 +55,7 @@ interface FooterProps {
 const Footer = (props: FooterProps) => {
     return (
         <div className="flex items-center justify-between px-5 pb-5">
-            <span className="text-xl font-bold text-white">{props.price.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })}</span>
+            <span className="text-xl font-bold text-white">{props.price.toLocaleString('id-ID', { style: 'currency', currency: 'USD' })}</span>
             <div className="w-15">
                 <Button title="Add to Cart" variant="bg-blue-600" onClick={props.addToCart}/>
             </div>
